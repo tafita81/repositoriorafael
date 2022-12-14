7a6d03b60e030c6c2d59699c963b4f7f4399d2cf
tafita81 <tafita81>
2022-12-14 23:19:28 +0000

    CASE WHEN

--- notebookrafael1.sql
+++ notebookrafael1.sql
@@ -58,3 +58,43 @@ where descsituacao IN ('canceled','shipped')
 
 and year(dtpedido) = '2018'
 and datediff (dtestimativaentrega,dtaprovado) > 30
+
+-- COMMAND ----------
+
+SELECT *,
+       CASE WHEN descuf = 'SP' THEN 'PAULISTA'  END AS DESCNACIONALIDADE
+FROM silver_olist.CLIENTE
+
+
+-- COMMAND ----------
+
+SELECT *,
+       CASE WHEN descuf = 'SP' THEN 'PAULISTA' 
+       ELSE 'OUTROS'
+        END AS DESCNACIONALIDADE
+       
+FROM silver_olist.CLIENTE
+
+-- COMMAND ----------
+
+SELECT *,
+       CASE WHEN descuf = 'SP' THEN 'PAULISTA' 
+            WHEN descuf = 'RJ' THEN 'FLUMINENSE' 
+       
+       ELSE 'OUTROS'
+        END AS DESCNACIONALIDADE
+       
+FROM silver_olist.CLIENTE
+
+-- COMMAND ----------
+
+SELECT *,
+       CASE WHEN descuf = 'SP' THEN 'PAULISTA' 
+            WHEN descuf = 'RJ' THEN 'FLUMINENSE' 
+            WHEN descuf = 'PR' THEN 'PARANAENSE' 
+       
+       ELSE 'OUTROS'
+        
+        END AS DESCNACIONALIDADE
+       
+FROM silver_olist.CLIENTE
